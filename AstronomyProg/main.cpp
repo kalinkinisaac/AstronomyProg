3ed9c62d117a988183eae0a3db4a40a7a217c69c
Isaac Kalinkin <isaackalinkin@macbook-air-isaac.local>
2016-07-02 15:56:45 +0300

    Initial Commit

---
+++ AstronomyProg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AstronomyProg
+//
+//  Created by Isaac Kalinkin on 02.07.16.
+//  Copyright © 2016 FatAndIsaac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
